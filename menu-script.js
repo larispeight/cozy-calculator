554e5d98834c842c0d2c70a2a78c5af25b296c8b
Hayspy <lari.speight@gmail.com>
2025-09-11 17:54:49 -0700

    feat: add toggle function to button; -changes on html to support toggle -js script "add" "remove" id function -css styling fix

--- menu-script.js
+++ menu-script.js
@@ -18,10 +18,19 @@ function updateZoom() {
   target.style.zoom = zoomLevel;
 }
 
-
 // night-mode
 const toggle = document.getElementById("checkbox")
 
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("night-mode");
+});
+
+// Log Tab
+const visorToggleBtn = document.getElementById('toggleLog');
+
+visorToggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('visor-open'); 
+  visorToggleBtn.textContent = document.body.classList.contains('visor-open') 
+      ? 'Hide Log' 
+      : 'Log Tab';
 });
